168890cb0dc1ad7d4ec4e63010ed60a44780a862
CaioRibeiro94 <54272601+CaioRibeiro94@users.noreply.github.com>
2023-11-04 12:58:50 -0300

    Add files via upload
    Modal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// MENU HAMB
+
 const btnMobile = document.getElementById('btn-mbl');
 
 function toggleMenu(event){
@@ -13,6 +15,7 @@ function toggleMenu(event){
     }
 }
 
+//MENU ROL
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
@@ -22,3 +25,18 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('.menu-bg')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+
+// MODAL
+
+function abrirM(){
+    const modal = document.getElementById('janela-modal');
+    modal.classList.add('abrir');
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id =='janela-modal'){
+            modal.classList.remove('abrir');
+        }
+    });
+
+}
